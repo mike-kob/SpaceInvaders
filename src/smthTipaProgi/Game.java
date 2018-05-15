948a157abeddfa5e2c0b92e318fcb2035f4851b7
mike-kob <37714867+mike-kob@users.noreply.github.com>
2018-05-15 23:11:32 +0300

    background and constants

--- src/smthTipaProgi/Game.java
+++ src/smthTipaProgi/Game.java
@@ -1,6 +1,5 @@
 package smthTipaProgi;
 
-import java.awt.GridLayout;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -21,22 +20,8 @@ public class Game {
 	static JLayeredPane lp = frame.getLayeredPane();;
 	static Rocket fighter;
 	static JPanel grid;
-	
-	public static Integer BACK_LAYER = 1;
-	public static Integer ROCKET_LAYER = 2;
-	public static Integer ALIEN_LAYER = 3;
-<<<<<<< HEAD
-	public static Integer DEFENCE_LAYER = 4;
-=======
-	public static Integer BOMB_LAYER = 4;
-	
-	
->>>>>>> ea32164b24a4931745c645416fdfebda463a2bfa
 
-	private static int ALIEN_ROWS = 5;
-	private static int ALIEN_COLUMNS = 7;
-
-	static Alien[][] aliens = new Alien[ALIEN_ROWS][ALIEN_COLUMNS];
+	static Alien[][] aliens = new Alien[Constants.ALIEN_ROWS][Constants.ALIEN_COLUMNS];
 	public static Set<Bomb> bombs = Collections.newSetFromMap(new ConcurrentHashMap<Bomb, Boolean>());
 
 	public static void main(String[] args) throws InvocationTargetException, InterruptedException {
@@ -44,16 +29,7 @@ public class Game {
 			public void run() {
 				drawEverything();
 				frame.addKeyListener(keyL);
-				
-			
-				
-		
-				
-				
-				
-				
-				
-				
+
 			}
 		});
 	}
@@ -68,36 +44,23 @@ public class Game {
 		l.setLocation(0, 0);
 		l.setVisible(true);
 		l.setSize(1280, 980);
-		lp.add(l, BACK_LAYER);
-		
+		lp.add(l, Constants.BACK_LAYER);
 
-
-		
 		fighter = new Rocket(0, 770);
-		lp.add(fighter, Game.ROCKET_LAYER);
+		lp.add(fighter, Constants.ROCKET_LAYER);
 
 		grid = new JPanel(null);
-		/*
-		for (int i = 0; i < ALIEN_ROWS; i++) {
-			for (int j = 0; j < ALIEN_COLUMNS; j++) {
-				Alien cur = new Alien();
-				cur.setLocation(i*30, j*30);
-				aliens[i][j] = cur;
-				grid.add(cur);
-			}
-		}*/
-		
+
 		JLabel d = new JLabel(new ImageIcon("res/defence.png"));
 		d.setLocation(50, 570);
-		d.setSize(250,200);
-		lp.add(d, DEFENCE_LAYER);
-		
-		
+		d.setSize(250, 200);
+		lp.add(d, Constants.DEFENCE_LAYER);
+
 		grid.setOpaque(true);
-		grid.setSize(700,300);
-		grid.setLocation((frame.getWidth()-grid.getWidth())/2, 50);
+		grid.setSize(700, 300);
+		grid.setLocation((frame.getWidth() - grid.getWidth()) / 2, 50);
 		grid.setVisible(true);
-		lp.add(grid, Game.ALIEN_LAYER);
+		lp.add(grid, Constants.ALIEN_LAYER);
 	}
 
 	static KeyListener keyL = new KeyAdapter() {
@@ -107,26 +70,21 @@ public class Game {
 
 			if (key == KeyEvent.VK_LEFT) {
 				fighter.left();
-				
+
 			} else if (key == KeyEvent.VK_RIGHT) {
 				fighter.right();
-			}
-			else if (key== KeyEvent.VK_ENTER) {
-				
-						Bomb bomb = new Bomb(fighter.getX());
-						System.out.println(fighter.getX());
-						lp.add(bomb,Game.BOMB_LAYER);
-						bombs.add(bomb);
-
-					
-				
+			} else if (key == KeyEvent.VK_SPACE) {
+				Bomb bomb = new Bomb(fighter.getX());
+				System.out.println(fighter.getX());
+				lp.add(bomb, Constants.BOMB_LAYER);
+				bombs.add(bomb);
 			}
 		}
 
 	};
-	
+
 	public static void updateAll(Set<? extends Updatable> elements) {
-		System.out.println("huh");
+
 		for (Updatable temp : elements) {
 			temp.update();
 		}
